4d41d90784aff42769d4d542f59f75af1702e828
marcellus97 <marcellusm.mohanna@gmail.com>
2021-11-14 16:46:27 -0500

    first draft. Note mobile responsive yet

--- server.js
+++ server.js
@@ -11,8 +11,31 @@ app.engine('handlebars', expressHandlebars({
     
 }));
 
+app.use('/static', express.static('static'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+
+// handle 404
+app.use((req, res) => {
+    res.status(404);
+  
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
+  
+    // respond with json
+    if (req.accepts('json')) {
+      res.json({ error: 'Not found' });
+      return;
+    }
+  
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
 
 app.listen(4000);
